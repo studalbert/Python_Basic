e39a75df1ef2bce06daee9169b8b198ee1892c43
alber <albert100098@gmail.com>
2024-09-20 17:29:22 +0500

    added m14 homework

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,1 +1,18 @@
 # TODO здесь писать код
+numbers = int(input('Элементов в списке: '))
+old_list = []
+for i in range(numbers):
+    num = int(input('Введите число: '))
+    old_list.append(num)
+print('Изначальный список:', old_list)
+shift = int(input('Сдвиг: '))
+new_list = []
+for _ in range(len(old_list)):
+    new_list.append(old_list[(_ - shift) % len(old_list)])
+    # new_list.append(old_list[(len(old_list) - i - shift) % len(old_list)])
+
+print('Сдвинутый список:', new_list)
+
+
+
+
--- Module15/09_reverse_analysis/main.py
+++ Module15/09_reverse_analysis/main.py
@@ -2,3 +2,15 @@
 
 # Список чисел для работы (итоговый алгоритм проверьте на разных списках, придуманных самостоятельно):
 numbers_list = [7, 14, 3, 18, 21, 10, 9, 6]
+print('Начальный список:', numbers_list)
+#1
+print('Отсортированный список:', end = ' ')
+for i in range(-1, -len(numbers_list)-1, -1):
+    if numbers_list[i] % 2 == 0:
+        print(numbers_list[i], end = ' ')
+#2
+print()
+print('Отсортированный список:', end = ' ')
+for i in range(len(numbers_list)):
+    if numbers_list[len(numbers_list) - i - 1] % 2 == 0:
+        print(numbers_list[len(numbers_list) - i - 1], end = ' ')
