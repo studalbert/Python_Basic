e4707033c7a05d68ba75e843336354980cdd3e9e
alber <albert100098@gmail.com>
2024-09-23 14:44:59 +0500

    added m17 homework

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,8 @@
 # TODO здесь писать код
+import random
+first_team = [round(random.uniform(5,10),2) for i in range(20)]
+second_team = [round(random.uniform(5,10),2) for i in range(20)]
+print('Первая команда:', first_team)
+print('Вторая команда:', second_team)
+winners = [first_team[i] if first_team[i] > second_team[i] else second_team[i] for i in range(20)]
+print('Победители тура:', winners)
--- Module17/08_caesar_cipher/main.py
+++ Module17/08_caesar_cipher/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+text = input('Введите текст: ')
+shift = int(input('Введите сдвиг: '))
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+result = ''
+for sym in text:
+  if sym in alphabet:
+    result += alphabet[(alphabet.index(sym) + shift) % len(alphabet)]
+  else:
+    result += sym
+print(result)
