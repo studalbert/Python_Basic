ca7ebb5c949d8c7538557408291026eaa2b5a987
alber <albert100098@gmail.com>
2024-10-02 21:40:01 +0500

    added m23 homework

--- Module23/02_lucky_number/main.py
+++ Module23/02_lucky_number/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+import random
+lst = list(range(13))
+summ = 0
+while summ < 777:
+    try:
+        num = int(input('Введите число: '))
+        summ += num
+        if random.choice(lst) == 0:
+            raise BaseException('Вас постигла неудача!')
+        with open('out_file.txt', 'a') as out_file:
+            out_file.write(f'{str(num)}\n')
+
+    except ValueError:
+        print('Ошибка. Пожалуйста, введите корректное число.')
+
+print('Вы успешно выполнили условие для выхода из порочного цикла!')
