63edbb9f3e1d2d928e8cddae3797cc1193086aa1
alber <albert100098@gmail.com>
2024-10-09 15:44:30 +0500

    added m26 homework

--- Module26/05_logs_processing/main.py
+++ Module26/05_logs_processing/main.py
@@ -1,14 +1,26 @@
 # TODO здесь писать код
-
+import os
 
 # При помощи модуля os (и функции join) сформируйте пути до файлов work_logs.txt и output.txt в папке data
 # (output.txt может не быть в папке data, но его нужно будет там создать, при помощи кода)
-input_file_path = ...
-output_file_path = ...
+input_file_path = os.path.abspath(os.path.join('data', 'work_logs.txt'))
+output_file_path = os.path.abspath(os.path.join('data', 'output.txt'))
 # Документация по join https://docs-python.ru/standart-library/modul-os-path-python/funktsija-join-modulja-os-path/
 
 # Не забудьте проверить наличие файлов перед тем как начать работу с ними
 # https://docs-python.ru/standart-library/modul-os-path-python/funktsija-exists-modulja-os-path/
+
+def error_log_generator(path_log_file):
+    if os.path.exists(path_log_file):
+        with open(path_log_file, 'r', encoding='utf-8') as file:
+            for line in file:
+                if 'ERROR' in line:
+                    yield line
+
+    else:
+        raise FileNotFoundError('Такого файла не существует')
+
+
 
 with open(output_file_path, 'w') as output:
     for error_line in error_log_generator(input_file_path):
