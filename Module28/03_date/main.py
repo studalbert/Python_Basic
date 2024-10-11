c5098a836ba694b7c5126878d933cbb6b0c3fdfc
alber <albert100098@gmail.com>
2024-10-11 23:38:44 +0500

    added m28 homework

--- Module28/03_date/main.py
+++ Module28/03_date/main.py
@@ -1,1 +1,39 @@
 # TODO здесь писать код
+from datetime import datetime
+class Date:
+    _date = None
+
+    def __str__(self) -> str:
+        date_lst = self._date.split('-')
+        return f'День: {date_lst[0]}\tМесяц: {date_lst[1]}\tГод: {date_lst[2]}'
+
+
+    @property
+    def date(self)-> str:
+        return self._date
+
+    @date.setter
+    def date(self, date) -> None:
+        self._date = date
+
+    @classmethod
+    def is_date_valid(cls, date_str: str) -> bool:
+        date_format = '%d-%m-%Y'
+        try:
+            if datetime.strptime(date_str, date_format):
+                return True
+        except Exception:
+            return False
+
+    @classmethod
+    def from_string(cls, date_str:str) -> 'Date':
+        if not cls.is_date_valid(date_str):
+            raise ValueError('Некорректная дата')
+        cls._date = date_str
+        return Date()
+
+date = Date.from_string('12-12-2077')
+print(date)
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('40-12-2077'))
+
--- Module28/04_request_caching/main.py
+++ Module28/04_request_caching/main.py
@@ -1,5 +1,70 @@
 # TODO здесь писать код
 
+class LRUCache:
+    """
+    класс LRU Cache,
+    который хранит ограниченное количество объектов и,
+    при превышении лимита,
+    удаляет самые давние (самые старые) использованные элементы.
+    Args and attrs:
+        capacity (int): вместимость списка _cache
+        _cache_counter (dict): словарь, ключи которого элементы из списка _cache, а значения - количество запросов к этим элементам
+        _cache (list): список элементов
+     """
+    def __init__(self,capacity:int) -> None:
+        self.capacity = capacity
+        self._cache_counter = dict()
+        self._cache = [None] * self.capacity
+
+    @property
+    def cache(self) -> tuple:
+        """ Геттер. возвращает самый старый элемент """
+        min_count = min(self._cache_counter.values())
+        for key in self._cache_counter:
+            if self._cache_counter[key] == min_count:
+                return key
+
+
+    @cache.setter
+    def cache(self, elem:tuple) -> None:
+        """ Сеттер. метод должен добавлять новый элемент в список  """
+        if None not in self._cache:
+            self.del_elem()
+        for i in range(len(self._cache)):
+            if not self._cache[i]:
+                self._cache[i] = elem
+                if elem not in self._cache_counter:
+                    self._cache_counter[elem] = 1
+                else:
+                    self._cache_counter[elem] += 1
+                break
+
+    def del_elem(self) -> None:
+        """ Метод удалает давно использованный элемент из списка  """
+        min_count = min(self._cache_counter.values())
+        for key in self._cache_counter:
+            if self._cache_counter[key] == min_count:
+                self._cache.remove(key)
+                self._cache_counter.pop(key)
+                break
+        self._cache.append(None)
+
+    def print_cache(self) -> None:
+        """" Печатает на экран элементы списка """
+        print('LRU Cache')
+        for key, value in self._cache:
+            print(f'{key} : {value}')
+        print()
+
+    def get(self, user_key:str) -> str:
+        """" Метод возвращает значение по ключу """
+        for key, value in self._cache:
+            if key == user_key:
+                self._cache_counter[(key, value)] += 1
+                return value
+        else:
+            return f'Такого ключа нет'
+
 
 
 # Создаем экземпляр класса LRU Cache с capacity = 3
@@ -21,3 +86,6 @@ cache.cache = ("key4", "value4")
 
 # Выводим обновленный кэш
 cache.print_cache()  # key2 : value2, key3 : value3, key4 : value4
+
+cache.cache = ("key5", "value5")
+cache.print_cache()
