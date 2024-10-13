1f923e775cbfd08abbb76592dc4c906d7eacf8bc
alber <albert100098@gmail.com>
2024-10-14 00:07:45 +0500

    added m29 homework

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,26 @@
 # TODO здесь писать код
+import functools
+from typing import Callable, Any
+
+app = {}
+def callback(event:str) -> Callable:
+    def decorator(func:Callable) -> Callable:
+        app[event] = func
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs) -> Any:
+            return func(*args, **kwargs)
+        return wrapped
+    return decorator
+
+@callback(event = '//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
