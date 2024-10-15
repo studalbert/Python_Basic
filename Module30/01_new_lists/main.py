055a3d056d25fadf58b2f17a3f040fd03ad99a4b
alber <albert100098@gmail.com>
2024-10-15 18:05:17 +0500

    added m30 homework

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -1,1 +1,21 @@
 # TODO здесь писать код
+from typing import List
+from functools import reduce
+
+if __name__ == '__main__':
+    floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
+    names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
+    numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
+
+    #Каждое число из списка floats возводится в третью степень и округляется до трёх знаков после запятой.
+    res_1: List[float] = list(map(lambda x: round(x**3, 3), floats))
+
+    #Из списка names берутся только те имена, в которых есть минимум пять букв.
+    res_2: List[str] = list(filter(lambda x: len(x) >= 5, names))
+
+    # Из списка numbers берётся произведение всех чисел.
+    res_3: int = reduce(lambda x, y: x * y, numbers)
+
+    print(f'{res_1}\n{res_2}\n{res_3}')
+
+
--- Module30/04_unique_cipher/main.py
+++ Module30/04_unique_cipher/main.py
@@ -1,8 +1,13 @@
 # TODO здесь писать код
+from collections import Counter
+def count_unique_characters(text:str) -> int:
+    sym_dict = Counter(text.lower())
+    result = sum(filter(lambda x: x if x == 1 else 0, sym_dict.values()))
+    return result
 
+if __name__ == '__main__':
+    # Пример использования:
+    message = "Today is a beautiful day! The sun is shining and the birds are singing."
 
-# Пример использования:
-message = "Today is a beautiful day! The sun is shining and the birds are singing."
-
-unique_count = count_unique_characters(message)
-print("Количество уникальных символов в строке:", unique_count)
+    unique_count = count_unique_characters(message)
+    print("Количество уникальных символов в строке:", unique_count)
